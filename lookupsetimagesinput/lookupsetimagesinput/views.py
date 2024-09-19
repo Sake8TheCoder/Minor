4af4f69972c8ac876fd1f8739b2748ca0aa203a3
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2024-09-19 13:27:09 +0530

    Create views.py

---
+++ lookupsetimagesinput/lookupsetimagesinput/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib import messages
+
+def indexPage(request):
+  if request.method == "POST":
+    pass
+
+return render(request,"index.html")
+
+
+  
