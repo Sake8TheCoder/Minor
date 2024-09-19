f8b6f6cc31c0993554a5e2c33268a6bb9d240af0
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2024-09-19 10:42:05 +0000

    .

--- lookupsetimagesinput/lookupsetimagesinput/urls.py
+++ lookupsetimagesinput/lookupsetimagesinput/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import indexPage
+from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('', indexPage),
+    path('LookUp/', LookUp),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
--- lookupsetimagesinput/lookupsetimagesinput/views.py
+++ lookupsetimagesinput/lookupsetimagesinput/views.py
@@ -2,10 +2,25 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.contrib import messages
 
-def indexPage(request):
-  if request.method == "POST":
-    pass
 
+def LookUp(request,context):
+  return render(request,"LookUp.html",context = context)
+
+def indexPage(request):
+  messages.get_messages(request)._loaded_messages.clear()
+
+  if request.method == "POST":
+    file1 = request.FILES.get("Image1")
+    file2 = request.FILES.get("Image2")
+    file3 = request.FILES.get("Image3")
+    file4 = request.FILES.get("Image4")
+    if not file1 and not file2 and not file3 and not file4:
+        messages.info(request,"No file has been entered")
+        redirect(indexPage)
+
+    Imgs = {"Images" : [file1 ,file2, file3, file4]}
+    return LookUp(request,Imgs)
+  
   return render(request,"index.html")
 
 
