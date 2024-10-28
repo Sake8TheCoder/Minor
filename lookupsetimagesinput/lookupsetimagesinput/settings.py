129d82cd7772cc81f37efed4040d9cb69a736512
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2024-10-28 17:36:05 +0530

    Deployment Ready

--- lookupsetimagesinput/lookupsetimagesinput/settings.py
+++ lookupsetimagesinput/lookupsetimagesinput/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)ajf#z2js%gr!$)4)97vey%!_3b#*i31d&t(d12ss#b7nx04qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
